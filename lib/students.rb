22fb1c9fb4c9b43af81d925157df6a11c356d489
Stefanie Davis <davisstefanie2010@gmail.com>
2019-11-03 01:35:33 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "select min(gpa) from students;"
 end
 
 def average_student_gpa
+  "select avg(gpa) from students;"
 end
 
 def total_tardies_for_all_students
